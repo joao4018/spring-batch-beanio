1ef5f8bbd9e7c7c6d569e2331bbfa3b7159a3e5b
joao4018 <joao4018@gmail.com>
2020-03-26 15:58:13 -0300

    Descomentando flow

--- src/main/java/com/example/demo/batch/flow/FlowSample.java
+++ src/main/java/com/example/demo/batch/flow/FlowSample.java
@@ -1,24 +1,24 @@
-//package com.example.demo.batch.flow;
-//
-//import org.springframework.batch.core.Step;
-//import org.springframework.batch.core.job.builder.FlowBuilder;
-//import org.springframework.batch.core.job.flow.Flow;
-//import org.springframework.batch.core.job.flow.support.SimpleFlow;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class FlowSample {
-//
-//    @Bean("flow")
-//    public Flow flow1(@Qualifier("stepReaderUsers") Step stepReaderUsers,
-//                      @Qualifier("stepReaderFileUsers") Step userStepReaderFile,
-//                      @Qualifier("stepWriterUsersDB") Step stepWriterUsersBD) {
-//        return new FlowBuilder<SimpleFlow>("flow")
-//                .start(stepReaderUsers)
-//                .next(userStepReaderFile)
-//                .next(stepWriterUsersBD)
-//                .build();
-//    }
-//}
+package com.example.demo.batch.flow;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlowSample {
+
+    @Bean("flow")
+    public Flow flow1(@Qualifier("stepReaderUsers") Step stepReaderUsers,
+                      @Qualifier("stepReaderFileUsers") Step userStepReaderFile,
+                      @Qualifier("stepWriterUsersDB") Step stepWriterUsersBD) {
+        return new FlowBuilder<SimpleFlow>("flow")
+                .start(stepReaderUsers)
+                .next(userStepReaderFile)
+                .next(stepWriterUsersBD)
+                .build();
+    }
+}
